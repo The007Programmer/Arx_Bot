1bdaf4986a58f8ed7cf1d30107b7d06457778f83
MilkshakeTheCoder <>
2021-06-02 02:57:18 +0000

    merged warns with moderation

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -6,6 +6,8 @@ import asyncio
 import discord
 from discord.ext import commands, tasks
 from dateutil.relativedelta import relativedelta
+from cogs import utils
+from cogs.utils.util import Pag
 
 time_regex = re.compile("(?:(\d{1,5})(h|s|m|d))+?")
 time_dict = {"h": 3600, "s": 1, "m": 60, "d": 86400}
@@ -213,6 +215,94 @@ class Moderation(commands.Cog):
         )
         await ctx.send(embed=embed, delete_after=15)
 
+    @commands.command()
+    @commands.guild_only()
+    @commands.has_permissions(administrator=True)
+    async def warn(self, ctx, member: discord.Member, *, reason):
+        if member.id in [ctx.author.id, self.bot.user.id]:
+            return await ctx.send("You cannot warn yourself or the bot!")
+        
+        current_warn_count = len(
+            await self.bot.warns.find_many_by_custom(
+                {
+                    "user_id": member.id,
+                    "guild_id": member.guild.id
+                }
+            )
+        ) + 1
+        
+        warn_filter = {"user_id": member.id, "guild_id": member.guild.id, "number": current_warn_count}
+        warn_data = {"reason": reason, "timestamp": ctx.message.created_at, "warned_by": ctx.author.id}
+        
+        await self.bot.warns.upsert_custom(warn_filter, warn_data)
+        
+        embed = discord.Embed(
+            title="You are being warned:",
+            description=f"__**Reason**__:\n{reason}",
+            colour=discord.Colour.red(),
+            timestamp=ctx.message.created_at
+        )
+        embed.set_author(name=ctx.guild.name, icon_url=ctx.guild.icon_url)
+        embed.set_footer(text=f"Warn: {current_warn_count}")
+        
+        try:
+            await member.send(embed=embed)
+            await ctx.send("Warned that user in dm's for you.")
+        except discord.HTTPException:
+            await ctx.send(member.mention, embed=embed)
+            
+    @commands.command()
+    @commands.guild_only()
+    @commands.has_permissions(administrator=True)
+    async def warns(self, ctx, member: discord.Member):
+        warn_filter = {"user_id": member.id, "guild_id": member.guild.id}
+        warns = await self.bot.warns.find_many_by_custom(warn_filter)
+        
+        if not bool(warns):
+            return await ctx.send(f"Couldn't find any warns for: `{member.display_name}`")
+        
+        warns = sorted(warns, key=lambda x: x["number"])
+        
+        pages = []
+        for warn in warns:
+            description = f"""
+            Warn Number: `{warn['number']}`
+            Warn Reason: `{warn['reason']}`
+            Warned By: <@{warn['warned_by']}>
+            Warn Date: {warn['timestamp'].strftime("%I:%M %p %B %d, %Y")}
+            """
+            pages.append(description)
+        
+        await Pag(
+            title=f"Warns for `{member.display_name}`",
+            colour=0xCE2029,
+            entries=pages,
+            length=1
+        ).start(ctx)
+
+    @commands.command(aliases=["delwarn", "dw"])
+    @commands.has_role(566132980687568896)
+    @commands.guild_only()
+    async def deletewarn(self, ctx, member: discord.Member, warn: int = None):
+        """Delete a warn / all warns from a given member"""
+        filter_dict = {"user_id": member.id, "guild_id": member.guild.id}
+        if warn:
+            filter_dict["number"] = warn
+
+        was_deleted = await self.bot.warns.delete_by_custom(filter_dict)
+        if was_deleted and was_deleted.acknowledged:
+            if warn:
+                return await ctx.send(
+                    f"I deleted warn number `{warn}` for `{member.display_name}`"
+                )
+
+            return await ctx.send(
+                f"I deleted `{was_deleted.deleted_count}` warns for `{member.display_name}`"
+            )
+
+        await ctx.send(
+            f"I could not find any warns for `{member.display_name}` to delete matching your input"
+        )
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
